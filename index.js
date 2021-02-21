7e7ff60b078c2fa47cc4b65cb9e59bc9c4d396d6
Jeremias <jerefigueiredo@gmail.com>
2021-02-21 16:56:11 -0300

    added js

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        let l=document.getElementById("log-btn");
-        let s=document.getElementById("sign-btn");
-        console.log(l,s);
-        l.remove();
-        s.remove();
-        let logout = document.createElement('Button');
-        logout.className='btn btn-secondary btn-lg';
-        logout.innerText="Log Out";
-        logout.onclick=function () {
-            bye();
-        };
-        let container = document.getElementById('registration');
-        container.appendChild(logout);
+        location.replace("dash.html");
     }
   });
-
-function update(){
-    this.innerHTML++;
-    this.style.color = "red";
-}
-function updateadd(){
-    db.collection('clicks').get().then(snapshot=>{
-        let c = snapshot.docs[0].data().clicks;
-        console.log(c);
-        db.collection('clicks').doc('global').update({clicks:c+1}).then(function() {
-            location.replace("index.html");
-        })
-    })
-}
 
 const signupform = document.getElementById('form-signup');
 if(signupform!=null){
